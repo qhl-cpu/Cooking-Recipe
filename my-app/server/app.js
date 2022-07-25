d6d86ac3ab46bc841048b220877fb5a7868a06a7
Haoliang Qi <907225883wxl@gmail.com>
2022-07-25 04:06:25 -0700

    First commit

--- my-app/server/app.js
+++ my-app/server/app.js
@@ -4,7 +4,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var reviewRouter = require('./routes/reviews');
 var cors = require('cors');
@@ -31,7 +30,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+
 app.use('/users', usersRouter);
 app.use('/reviews', reviewRouter);
 
